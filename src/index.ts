82621f54934cef3ae1d22429a2d85a2d7d597910
Gabriel <gabrielhtres@gmail.com>
2023-06-13 17:31:40 -0300

    Morgan adicionado como dependência da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import rootRoute from './Routes/index';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3030;
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/', rootRoute);
 
