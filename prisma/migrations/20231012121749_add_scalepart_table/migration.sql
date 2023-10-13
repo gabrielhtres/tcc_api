f2038a4c891a8252b25203e83f5a0278605d5abd
Gabriel <gabrielhtres@gmail.com>
2023-10-13 17:14:10 -0300

    Criada tabela de partes da escala

---
+++ prisma/migrations/20231012121749_add_scalepart_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "scale_phase" (
+    "id" SERIAL NOT NULL,
+    "percentage" INTEGER NOT NULL,
+    "scale_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "scale_phase_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "scale_phase" ADD CONSTRAINT "scale_phase_scale_id_fkey" FOREIGN KEY ("scale_id") REFERENCES "scale"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
