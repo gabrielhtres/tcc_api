3b3d4bc50e0e03d2ae890e1aae40ad89e65df20e
Gabriel <gabrielhtres@gmail.com>
2023-11-15 15:28:47 -0300

    Ajustados campos

---
+++ prisma/migrations/20231114201216_change_coordinates/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "plot" ALTER COLUMN "x_coordinate" SET DATA TYPE TEXT,
+ALTER COLUMN "y_coordinate" SET DATA TYPE TEXT;
---
+++ prisma/migrations/20231114222306_change_percentage/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "scale_part" ALTER COLUMN "percentage" SET DATA TYPE DECIMAL;
